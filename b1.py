8dbc5df951c57e592e3324920d21f88f6c97ff84
= <=>
2023-07-25 19:06:02 +0700

    update

--- b1.py
+++ b1.py
@@ -1,9 +1,5 @@
-a = int(input("Nhập giá trị cần kiểm tra "))
-if a > 10:
-    print("Giá trị lớn hơn 10 và là số dương")
-elif 0 < a <= 10:
-    print("Giá trị nhỏ hơn hoặc bằng 10 và là số dương")
-elif a < 0:
-    print("Giá trị là số âm")
-else:
-    print("Giá trị bằng 0")
+i = int(input("Nhập số: "))
+x = 1
+while x <= i:
+    x += 1
+print(x)
